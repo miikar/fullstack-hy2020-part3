c2c551aa93cb01c4b820667ca74075bf5bfce3eb
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2021-06-03 15:31:50 +0300

    [part3] Completed 3.9

---
+++ README.rst
@@ -0,0 +1,13 @@
+Tasks:
+======
+- 3.1 DONE
+- 3.2 DONE
+- 3.3 DONE
+- 3.4 DONE
+- 3.5 DONE
+- 3.6 DONE
+- 3.7 DONE
+- 3.8 DONE
+- 3.9
+- 3.10
+- 3.11
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -99,6 +101,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)  
+})
