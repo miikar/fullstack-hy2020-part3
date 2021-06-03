a46680e4d81068b51dcdee605c0309ffcc40be32
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2021-06-03 11:32:32 +0300

    [part3] Completed 3.1-3.6

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let maxId = 10000
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  }
+]
+
+app.get('/info', (req, res) => {
+  msg = `
+    <p>Phonebook has ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  res.send(msg) 
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(400).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * maxId);
+}
+
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name is missing'
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  
+  res.json(person)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
